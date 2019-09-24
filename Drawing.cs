44bbde441ed6e463a7feb14625bc164c07049f19
Chase Austin <43153579+caustin3@users.noreply.github.com>
2019-09-23 19:48:06 -0600

    Add files via upload

---
+++ Drawing.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class Drawing : MonoBehaviour
+{
+    
+    public Material mat;
+    Vector3 Vertex_1, Vertex_2;       
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vertex_1 = new Vector3(0, 0, 0);
+        Vertex_2 = new Vector3(0, 0, 0);
+        mat = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 WorldPoint = new Vector3();
+            Vector3 work_ = new Vector3();
+            WorldPoint = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+            work_ = new Vector3(Input.mousePosition.x, Input.mousePosition.y,0f);
+            Vertex_1 = WorldPoint;
+            Debug.Log("and point" + WorldPoint.x + " and y:" + WorldPoint.y);
+
+            Draw_a_circle(10, Color.black, work_);
+        }
+       
+    }
+    void OnPostRender()
+    {
+
+        GL.PushMatrix();
+        //GL.Viewport(new Rect(.5f, 0, 1, 1));
+        mat.SetPass(0);
+
+        GL.Begin(GL.QUADS);
+        GL.Color(new Color(0, 0, 1f));
+        GL.Vertex3(0, 5f, 0);
+        GL.Color(new Color(0.25f, 0, 0.75f));
+        GL.Vertex3(5f, 10, 0);
+        GL.Color(new Color(0.50f, 0, .50f));
+        GL.Vertex3(10, 5f, 0);
+        GL.Color(new Color(0.75f, 0, .25f));
+        GL.Vertex3(5f, 0, 0);
+        GL.End();
+               
+        GL.PopMatrix();        
+        
+    }
+
+    void Draw_a_circle(int radis, Color new_color, Vector3 center)
+    {
+        Debug.Log("Draw_a_circle");
+        int check = 0;
+        GL.PushMatrix();
+
+        mat.SetPass(0);
+
+        GL.Begin(GL.TRIANGLE_STRIP);
+
+        GL.Color(new_color);
+        for (int a = 0; a < 361; a += 360 / 360)
+        {
+            if (check != 2)
+            {
+                double heading = (a * 3.1415926535) / 180;
+                float x_1 = (float)Math.Cos(heading) * radis;
+                float y_1 = (float)Math.Sin(heading) * radis;
+                GL.Vertex3(center.x + x_1, center.y + y_1, center.z);
+                check++;
+            }
+            else if (check == 2)
+            {
+                check = 0;
+                GL.Vertex3(center.x, center.y, center.z);
+            }
+        }
+        GL.End();
+        GL.PopMatrix();
+    }
+
+    void Draw_a_square(int length, Color new_color, Vector3 center)
+    {
+        float offset = (length / 2);
+        GL.PushMatrix();
+        mat.SetPass(0);
+        GL.Begin(GL.QUADS);
+        GL.Color(new_color);
+        GL.Vertex3(center.x - offset, center.y - offset, center.z);
+        GL.Vertex3(center.x + offset, center.y - offset, center.z);
+        GL.Vertex3(center.x + offset, center.y + offset, center.z);
+        GL.Vertex3(center.x - offset, center.y + offset, center.z);
+        GL.End();
+        GL.PopMatrix();
+    }
+
+    void Draw_a_triangle(int length, Color new_color, Vector3 center)
+    {
+        float offset_x = (length / 2);
+        float offset_y = (float)((length * Math.Sqrt(3)) / 2) / 2;
+        GL.PushMatrix();
+        mat.SetPass(0);
+        GL.Begin(GL.TRIANGLES);
+        GL.Color(new_color);
+        GL.Vertex3(center.x, center.y - offset_y, center.z);
+        GL.Vertex3(center.x + offset_x, center.y + offset_y, center.z);
+        GL.Vertex3(center.x - offset_x, center.y + offset_y, center.z);
+        GL.End();
+        GL.PopMatrix();
+    }
+
+}
---
+++ Drawing_the_toolbar.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class Drawing_the_toolbar : MonoBehaviour
+{
+    public Material toolbar_canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+    void OnPostRender()
+    {
+        toolbar_canvas = GetComponent<Renderer>().material;
+
+        GL.PushMatrix();
+        toolbar_canvas.SetPass(0);
+        GL.Begin(GL.LINE_STRIP);
+        GL.Color(Color.black);
+        GL.Vertex3(30, -100, 0);
+        GL.Vertex3(30, 100, 0);       
+        GL.End();
+        GL.PopMatrix();
+
+        Draw_a_circle(3, Color.black, new Vector3(-33, 70, 0));
+        Draw_a_circle(5, Color.black, new Vector3(-33, 60, 0));
+        Draw_a_circle(10, Color.black, new Vector3(-33, 44, 0));
+
+        Draw_a_square(5, Color.black, new Vector3(-10, 70, 0));
+        Draw_a_square(10, Color.black, new Vector3(-10, 60, 0));
+        Draw_a_square(20, Color.black, new Vector3(-10, 44, 0));
+
+        Draw_a_triangle(5, Color.black, new Vector3(15, 70, 0));
+        Draw_a_triangle(10, Color.black, new Vector3(15, 60, 0));
+        Draw_a_triangle(20, Color.black, new Vector3(15, 44, 0));
+
+
+        Draw_a_square(10, Color.red, new Vector3(-33, 0, 0));
+        Draw_a_square(10, Color.green, new Vector3(-23, 0, 0));
+        Draw_a_square(10, Color.blue, new Vector3(-13, 0, 0));
+        Draw_a_square(10, Color.yellow, new Vector3(-3, 0, 0));
+        Draw_a_square(10, Color.black, new Vector3(7, 0, 0));
+
+        GL.PushMatrix();
+        toolbar_canvas.SetPass(0);
+        GL.Begin(GL.QUADS);
+        GL.Color(Color.black);
+        GL.Vertex3(-33, -10, 0);
+        GL.Vertex3(7, -10, 0);
+        GL.Vertex3(7, -11, 0);
+        GL.Vertex3(-33, -11, 0);
+        GL.End();
+        GL.PopMatrix();
+
+        GL.PushMatrix();
+        toolbar_canvas.SetPass(0);
+        GL.Begin(GL.LINE_STRIP);
+        GL.Color(Color.black);
+        GL.Vertex3(-33,-15 , 0);
+        GL.Vertex3(-23,-15 , 0);
+        GL.Vertex3(-23,-25 , 0);
+        GL.Vertex3(-33,-25 , 0);
+        GL.Vertex3(-33, -15, 0);
+        GL.End();
+        GL.PopMatrix();
+    }
+
+    void Draw_a_circle(int radis,Color new_color, Vector3 center)
+    {
+        int check = 0;
+        GL.PushMatrix();
+
+        toolbar_canvas.SetPass(0);
+
+        GL.Begin(GL.TRIANGLE_STRIP);   
+        
+        GL.Color(new_color);       
+        for (int a = 0; a < 361; a += 360 / 360)
+        {
+            if (check != 2)
+            {
+                double heading = (a * 3.1415926535) / 180;
+                float x_1 = (float)Math.Cos(heading) * radis;
+                float y_1 = (float)Math.Sin(heading) * radis;
+                GL.Vertex3(center.x + x_1, center.y + y_1, center.z );
+                check++;
+            }
+            else if (check == 2)
+            {
+                check = 0;
+                GL.Vertex3(center.x, center.y, center.z);
+            }
+        }
+        GL.End();
+        GL.PopMatrix();
+    }
+
+    void Draw_a_square(int length, Color new_color, Vector3 center)
+    {
+        float offset = (length / 2);
+        GL.PushMatrix();
+        toolbar_canvas.SetPass(0);
+        GL.Begin(GL.QUADS);
+        GL.Color(new_color);
+        GL.Vertex3(center.x - offset, center.y- offset, center.z);
+        GL.Vertex3(center.x + offset, center.y - offset, center.z);
+        GL.Vertex3(center.x + offset, center.y + offset, center.z);
+        GL.Vertex3(center.x - offset, center.y + offset, center.z);
+        GL.End();
+        GL.PopMatrix();
+    }
+
+    void Draw_a_triangle(int length, Color new_color, Vector3 center)
+    {
+        float offset_x = (length / 2);
+        float offset_y = (float) ((length* Math.Sqrt(3)) /2)/2;
+        GL.PushMatrix();
+        toolbar_canvas.SetPass(0);
+        GL.Begin(GL.TRIANGLES);
+        GL.Color(new_color);
+        GL.Vertex3(center.x, center.y + offset_y, center.z);
+        GL.Vertex3(center.x + offset_x, center.y - offset_y, center.z);
+        GL.Vertex3(center.x - offset_x, center.y - offset_y, center.z);
+        GL.End();
+        GL.PopMatrix();
+    }
+}
